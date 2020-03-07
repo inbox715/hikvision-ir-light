ec6f18d24b2c300fb6fe416da309af63a7056231
inbox715 <43434574+inbox715@users.noreply.github.com>
2020-03-07 17:38:54 +0400

    Update ir_on.py

--- ir_on.py
+++ ir_on.py
@@ -2,6 +2,6 @@ import requests
 from requests.auth import HTTPDigestAuth
 
 x= requests.put('http://192.168.1.66/ISAPI/image/channels/1/IrcutFilter',
-data='<IrcutFilter xmlns="http://www.hikvision.com/ver20/XMLSchema" version="2.0"><IrcutFilterType>night</IrcutFilterType><nightToDayFilterLevel>4</nightToDayFilterLevel><nightToDayFilterTime>5</nightToDayFilterTime></IrcutFilter>', auth=HTTPDigestAuth('admin', '8024912mM'))
+data='<IrcutFilter xmlns="http://www.hikvision.com/ver20/XMLSchema" version="2.0"><IrcutFilterType>night</IrcutFilterType><nightToDayFilterLevel>4</nightToDayFilterLevel><nightToDayFilterTime>5</nightToDayFilterTime></IrcutFilter>', auth=HTTPDigestAuth('admin', 'pass'))
 
 print(x.content)
